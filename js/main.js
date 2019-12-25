5f044a70330d4d314ae47d14d55f25ea6ef6768c
Violetta <sarkisyan.violetta.2013@gmail.com>
2019-12-25 07:11:36 +0300

    add script jqwery

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    const modal = document.querySelector('.modal');
-    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-    const closeBtn = document.querySelector('.modal__close');
-    const switchModal = () => {
-        modal.classList.toggle('modal--visible');
-    }
+// document.addEventListener("DOMContentLoaded", function(event) {
+//     const modal = document.querySelector('.modal');
+//     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//     const closeBtn = document.querySelector('.modal__close');
+//     const switchModal = () => {
+//         modal.classList.toggle('modal--visible');
+//     }
    
-    modalBtn.forEach(element => {
-        element.addEventListener('click', switchModal);
-    });
-    closeBtn.addEventListener('click', switchModal);
-});
-
-
-// $(document).ready(function(){
-//     var modal = $('.modal'),
-//         modalBtn = $('[data-toggle=modal]'),
-//         closeBtn = $('.modal__close');
-//         console.log(modal);
-//         console.log(modalBtn);
-//         console.log(closeBtn);
-        
+//     modalBtn.forEach(element => {
+//         element.addEventListener('click', switchModal);
+//     });
+//     closeBtn.addEventListener('click', switchModal);
 // });
+
+
+$(document).ready(function () {
+    var modal = $('.modal'),
+        modalBtn = $('[data-toggle="modal"]'),
+        closeBtn = $('.modal__close');
+        modalBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+        });
+        closeBtn.on('click', function () {
+            modal.toggleClass('modal--visible');
+
+        });
+        // console.log(modal);
+        // console.log(modalBtn);
+        // console.log(closeBtn);
+        
+});
